3ff8482dd32d822c328b67c992149f15b3ea9f81
Corinna Bürger <corinna_buerger@yahoo.de>
2020-04-03 23:39:24 +0200

    initial commit

---
+++ yt_no_recommendations.js
@@ -0,0 +1,24 @@
+/*
+ * Author: Corinna Buerger
+ * Date: 04/03/2020
+ * Repo: CorinnaBuerger/yt_no_recommendations
+ */
+const MODE = "debugging";
+
+function hideElements(mode) {
+    const startPageSuggestions = document.getElementById("contents");
+    const videoPlayerSuggestions = document.querySelector("#secondary");
+    
+    const elementsToHide = [startPageSuggestions, videoPlayerSuggestions];
+    
+    if (mode === "debugging") {
+        console.log(elementsToHide);
+    }
+    
+    const numElementsToHide = elementsToHide.length;
+    for (let i = 0; i < numElementsToHide; i++) {
+        elementsToHide[i].style.display = "none";
+    }
+}
+
+hideElements(MODE);
